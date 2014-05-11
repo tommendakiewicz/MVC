4bbd222da90d6290ccf0531127557a6f48af5467
Tom Mendakiewicz <tom.mendakiewicz@gmail.com>
2014-05-10 21:29:53 -0400

    My first MVC app
    Small program to learn the basics of MVC

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.greeting = hour < 12 ? "Good morning" : "Good afternoon";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+          return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+          if (ModelState.IsValid)
+          {
+            // TODO: Email response to the party organiser
+            return View("Thanks", guestResponse);
+          } else
+          {
+            // validation error
+            return View();
+          }
+        }
+
+    }
+}
